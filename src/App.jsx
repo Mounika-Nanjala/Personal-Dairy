288eb5092532782bb2a79a77ad7679bf1438638a
Alexander  von der Bey <alexandervonderbey@gmail.com>
2025-02-03 18:17:09 +0100

    added diary entry component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./index.css";
 import Footer from "./components/Footer.jsx";
+import DiaryEntry from "./components/DiaryEntry.jsx";
 
 function App() {
   return (  
   <div className="bg-slate-200">
+    <DiaryEntry />
     <Footer />
   </div>
   );
