82045624bdb480e1373ef61e4d456d31d0c4e2ac
Anne Martinez <martinezannemerlyn@gmail.com>
2025-02-06 13:09:25 +0100

    updated modal to complement dark and light mode

--- src/components/EntryModal.jsx
+++ src/components/EntryModal.jsx
@@ -7,17 +7,23 @@ const EntryModal = ({ entry, onClose, updateEntry }) => {
   const [currentEntry, setCurrentEntry] = useState(entry);
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 dark:bg-gray-900 dark:bg-opacity-50">
       {/* Container EntryModal & AddEntryModal */}
-      <div className={`bg-white p-4 rounded-lg shadow-lg max-w-lg w-full transition-opacity duration-300 ${showEditModal ? "opacity-0 pointer-events-none" : "opacity-100"}`}>
-        <h2 className="text-xl font-bold">{currentEntry?.title}</h2>
-        <p>{currentEntry?.date}</p>
+      <div
+        className={`p-4 rounded-lg shadow-lg max-w-lg w-full transition-opacity duration-300 bg-white dark:bg-gray-800 ${
+          showEditModal ? "opacity-0 pointer-events-none" : "opacity-100"
+        }`}
+      >
+        <h2 className="text-xl font-bold text-gray-900 dark:text-gray-100">
+          {currentEntry?.title}
+        </h2>
+        <p className="text-gray-700 dark:text-gray-300">{currentEntry?.date}</p>
         <img
           src={currentEntry.imageUrl}
           alt={currentEntry.title}
           className="w-full h-48 object-cover rounded-lg my-2"
         />
-        <p>{currentEntry?.desc}</p>
+        <p className="text-gray-700 dark:text-gray-300">{currentEntry?.desc}</p>
 
         {/* Buttons */}
         <div className="flex justify-between mt-4">
